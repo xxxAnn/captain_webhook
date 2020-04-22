54fed44d32cfe68681cf15a932d902c760d93b38
Kyando2 <34218031+Kyando2@users.noreply.github.com>
2020-04-22 03:53:55 +0000

    Add files via upload

--- config.py
+++ config.py
@@ -1,16 +1,16 @@
-token_const = "NzAxOTU4NDc3NjUxNzcxNDAz.Xp5POA.yjVjWeOcEIhWHBVfrvYl524RItc"
 prefix_var = "p!"
 admin_list = [489561787717648394, 497582475091116042, 331431342438875137]
 topic_message = "{0}"
+from secret import token_const
 
 
 class bot_config:
     def __init__(self, token, prefix, admins, default_topic):
-        self.token = token
         self.prefix = prefix
+        self.token = token
         self.admins = admins
         self.default_topic_message = default_topic
         
         
 def get_config():
-    return bot_config(token=token_const, prefix=prefix_var, admins=admin_list, default_topic=topic_message)
+    return bot_config(token= token_const, prefix=prefix_var, admins=admin_list, default_topic=topic_message)
