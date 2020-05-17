8c8fcc98c21a433ebe056e86db7b982528fbbebb
Kyando <candlelightwax@gmail.com>
2020-05-18 05:38:41 +0900

    Update captain_webhook_main.py

--- captain_webhook_main.py
+++ captain_webhook_main.py
@@ -21,7 +21,7 @@ parser = WiktionaryParser()
 epoch = time.time()
 config = get_config()
 last_topic = -1
-# client.remove_command(help)
+client.remove_command(help)
 list_numbers_banned = []
 client = config.client
 initial_extensions = ['cogs.moderation', 'cogs.voice_cog', 'cogs.points_cog', 'cogs.miscellaneous', 'cogs.elections']
