1b5dbeeaba0f7196d2333fbf2c4387e62776af53
Kyando <candlelightwax@gmail.com>
2020-07-02 04:51:49 +0900

    hmm

--- cogs/Money.py
+++ cogs/Money.py
@@ -366,7 +366,7 @@ class Economy(commands.Cog):
             list1 = find_item_from_id(item['list1'])
             print(list1[0].name)
             text= "**{0}**, Requires Item: **{2}** __x{1}__".format(item["Result"], item["list2"][0], list1[0].name)
-            if len(list1)>1:
+            if list[1]:
                 text+= " and Item Id **{1}** __x{0}__".format(item["list2"][1], list[1].name)
             text+="\n"
             string+=text
